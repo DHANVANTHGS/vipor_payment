55bb9177a24e340bdaf56b3a2e0b5871d4a85a95
dhanvanthgs25@gmail.com <dhanvanthgs25@gmail.com>
2025-11-19 18:50:37 +0530

    model included

--- model/palm_model.js
+++ model/palm_model.js
@@ -1,13 +1,16 @@
 const ort = require("onnxruntime-node");
+const path = require("path");
 
 let session = null;
 
 async function loadPalmModel() {
     if (!session) {
-        session = await ort.InferenceSession.create("./model/palm_model.onnx");
+        const modelPath = path.join(__dirname, "palm_model.onnx");
+        session = await ort.InferenceSession.create(modelPath);
         console.log("Palm Model Loaded (ONNX)");
     }
     return session;
 }
 
+loadPalmModel();
 module.exports = loadPalmModel;
